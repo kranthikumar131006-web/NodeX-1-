11793bb964a7d094f9fc5a27958468937a6cc1d7
Manoj Lakkoju <manojlakkoju008@gmail.com>
2025-12-27 13:49:41 +0000

    remove the "create team"

--- src/app/(main)/hackathons/teams/page.tsx
+++ src/app/(main)/hackathons/teams/page.tsx
@@ -38,12 +38,6 @@ export default function HackathonTeamsPage() {
                 <Filter className="mr-2 h-4 w-4" />
                 Filters
             </Button>
-             <Button asChild>
-                <Link href="#">
-                  <Plus className="mr-2 h-4 w-4" />
-                  Create Team
-                </Link>
-            </Button>
           </div>
 
           <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
