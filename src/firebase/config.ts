f967dc71058a0bd489b7f2ec4a9564e7bc29e26e
Manoj Lakkoju <manojlakkoju008@gmail.com>
2025-12-27 14:55:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2508690281-90c36",
+  "appId": "1:740929568608:web:73aab988ad56d2f602e805",
+  "apiKey": "AIzaSyDOgAUgZ-7PWnFzST4MuAZZMGMVSIYBkbk",
+  "authDomain": "studio-2508690281-90c36.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "740929568608"
+};
