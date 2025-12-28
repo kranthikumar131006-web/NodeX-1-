a6250e5b3ff0346f3e76d7daa415279dd45318a6
Manoj Lakkoju <manojlakkoju008@gmail.com>
2025-12-28 07:38:23 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/(main)/freelancers/page.tsx
+++ src/app/(main)/freelancers/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { FreelancerCard } from '@/components/shared/freelancer-card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
